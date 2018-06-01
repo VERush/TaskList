6a0b35d7cc5745e4d20ba3b11228f53949181af7
VERush <Vicki@rushworld.com>
2018-06-01 13:22:47 -0400

    Fleshed out the POJOs

--- src/main/java/com/gc/TaskList/entity/Task.java
+++ src/main/java/com/gc/TaskList/entity/Task.java
@@ -1,5 +1,84 @@
 package com.gc.TaskList.entity;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "find_all_tasks", query = "select t from Task t")
 public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	int userid;
+	String task;
+	Date duedate;
+	boolean complete;
+
+	public Task() {
+
+	}
+
+	public Task(int id, int userid, String task, Date duedate, boolean complete) {
+		this.id = id;
+		this.userid = userid;
+		this.task = task;
+		this.duedate = duedate;
+		this.complete = complete;
+	}
+
+	public Task(int userid, String task, Date duedate, boolean complete) {
+		this.userid = userid;
+		this.task = task;
+		this.duedate = duedate;
+		this.complete = complete;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public Date getDuedate() {
+		return duedate;
+	}
+
+	public void setDuedate(Date duedate) {
+		this.duedate = duedate;
+	}
+
+	public boolean isComplete() {
+		return complete;
+	}
+
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+
+	@Override
+	public String toString() {
+		return "Task: " + id + ", userid: " + userid + ", task: " + task + ", duedate: " 
+				+ duedate + ", complete: " + complete;
+	}
 
 }
--- src/main/java/com/gc/TaskList/entity/User.java
+++ src/main/java/com/gc/TaskList/entity/User.java
@@ -1,5 +1,72 @@
 package com.gc.TaskList.entity;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
+@Entity
+@NamedQuery(name="find_all_users", query="select u from User u")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userid;
+	private String name;
+	private String email;
+	private String password;
+
+	public User() {
+
+	}
+
+	public User(int userid, String name, String email, String password) {
+		this.userid = userid;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public User(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User: " + userid + ", name: " + name + ", email: " + email + ", password: " + password;
+	}
+	
+
+	
 }
